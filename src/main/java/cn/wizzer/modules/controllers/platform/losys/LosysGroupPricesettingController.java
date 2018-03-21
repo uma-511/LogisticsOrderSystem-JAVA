1eb500394c364faf200d2c1763b22278bfa6583c
liangcr <472876921@qq.com>
2018-03-21 11:25:43 +0800

    #价格分组管理

--- src/main/java/cn/wizzer/modules/controllers/platform/losys/LosysGroupPricesettingController.java
+++ src/main/java/cn/wizzer/modules/controllers/platform/losys/LosysGroupPricesettingController.java
@@ -1,19 +1,29 @@
 package cn.wizzer.modules.controllers.platform.losys;
 
+import cn.apiclub.captcha.filter.image.TextureFilter;
 import cn.wizzer.common.annotation.SLog;
 import cn.wizzer.common.base.Result;
 import cn.wizzer.common.filter.PrivateFilter;
 import cn.wizzer.common.page.DataTableColumn;
 import cn.wizzer.common.page.DataTableOrder;
+import cn.wizzer.modules.models.losys.Lo_area;
+import cn.wizzer.modules.models.losys.Lo_group_pricesetting;
 import cn.wizzer.modules.models.losys.Lo_logistics;
+import cn.wizzer.modules.models.sys.Sys_menu;
 import cn.wizzer.modules.models.sys.Sys_unit;
+import cn.wizzer.modules.services.losys.LosysAreaService;
+import cn.wizzer.modules.services.losys.LosysGroupPricesettingService;
 import cn.wizzer.modules.services.losys.LosysLogisticsService;
 import cn.wizzer.modules.services.sys.SysMenuService;
 import cn.wizzer.modules.services.sys.SysUnitService;
 import cn.wizzer.modules.services.sys.SysUserService;
+import oracle.net.aso.a;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -41,61 +51,67 @@ import org.nutz.mvc.annotation.*;
 import com.mysql.fabric.xmlrpc.base.Data;
 
 
-
+/**
+ * 分组价格管理
+ * @author Lenovo
+ *
+ */
 
 @IocBean
-@At("/platform/losys/logistics")
+@At("/platform/losys/GroupPricesetting")
 @Filters({@By(type = PrivateFilter.class)})
-public class LosysLogisticsController {
+public class LosysGroupPricesettingController {
     private static final Log log = Logs.get();
     @Inject
-    private LosysLogisticsService logisticsService;
+    private LosysGroupPricesettingService groupPricesettingService;
 
     /**
-     * 访问物流公司模块首页
+     * 访问价格分组管理模块首页
      */
     @At("")
-    @Ok("beetl:/platform/losys/logistics/index.html")
+    @Ok("beetl:/platform/losys/GroupPricesetting/index.html")
     @RequiresAuthentication
-    public void index() {
+    public void index(HttpServletRequest req) {
+    	
+    	List<Lo_group_pricesetting> group_pricesettings = groupPricesettingService.query(Cnd.where("1", "=", "1").asc("opAt"));
     
+    	req.setAttribute("list", group_pricesettings);
     }
     
     /**
-     * 访问物流公司模块添加物流公司页面
+     * 访问价格分组管理模块添加价格分组页面
      */
     @At
-    @Ok("beetl:/platform/losys/logistics/add.html")
+    @Ok("beetl:/platform/losys/GroupPricesetting/add.html")
     @RequiresAuthentication
     public void add() {
     	
     }
 
     /**
-     * 访问物流公司模块修改物流公司页面
+     * 访问价格分组管理模块价格分组页面
      */
     @At("/edit/?")
-    @Ok("beetl:/platform/losys/logistics/edit.html")
-    @RequiresPermissions("logistics.manage.edit")
+    @Ok("beetl:/platform/losys/GroupPricesetting/edit.html")
+    @RequiresPermissions("groupPricesetting.manage.edit")
     public Object edit(String id,HttpServletRequest req) {
-    	Lo_logistics logistics = logisticsService.fetch(id);
-    	req.setAttribute("logistics", logistics);
-    	return logistics;
+    	Record group_pricesetting = groupPricesettingService.dao().fetch("lo_group_pricesetting", Cnd.where("id", "=", id));
+    	req.setAttribute("group_pricesetting", group_pricesetting);
+    	return group_pricesetting;
     }
     
     /**
-     * 添加物流公司
+     * 添加价格分组
      */
 	@At
     @Ok("json")
-    @RequiresPermissions("logistics.manage.add")
-    @SLog(tag = "添加物流公司", msg = "商品名称:${args[0].name}")
+    @RequiresPermissions("groupPricesetting.manage.add")
+    @SLog(tag = "添加价格分组", msg = "价格分组名id:${args[0].id}")
 	@AdaptBy(type = WhaleAdaptor.class)
-    public Object addDo(@Param("..") Lo_logistics logistics, HttpServletRequest req) {
+    public Object addDo(@Param("..") Lo_group_pricesetting group_pricesetting, HttpServletRequest req) {
         try {
-        	logistics.setOpAt((int)System.currentTimeMillis());
-        	logistics.setDelFlag(false);
-            logisticsService.insert(logistics);
+        	group_pricesetting.setOpAt((int)System.currentTimeMillis());
+            groupPricesettingService.insert(group_pricesetting);
             return Result.success("system.success");
         } catch (Exception e) {
             return Result.error("system.error");
@@ -105,23 +121,22 @@ public class LosysLogisticsController {
     
     
     /**
-     * 删除物流公司 
+     * 删除  价格分组
      */
     @At({"/delete/?", "/delete"})
     @Ok("json")
-    @RequiresPermissions("logistics.manage.delete")
-    @SLog(tag = "删除物流公司", msg = "ID:${args[2].getAttribute('id')}")
+    @RequiresPermissions("groupPricesetting.manage.delete")
     public Object delete(String oneId, @Param("ids") String[] ids, HttpServletRequest req) {
     	try {
         	if (ids != null && ids.length > 0) {
             	for (String string : ids) {
-            		//logisticsService.delete(string);
-            		logisticsService.dao().update(Lo_logistics.class, Chain.make("delFlag", true), Cnd.where("id","=",string));
+            		/*groupPricesettingService.delete(string);*/
+            		groupPricesettingService.dao().clear("lo_group_pricesetting", Cnd.where("id", "=", string));
             	}
                 req.setAttribute("id", org.apache.shiro.util.StringUtils.toString(ids));
             } else {
-            	//logisticsService.delete(oneId);
-            	logisticsService.dao().update(Lo_logistics.class, Chain.make("delFlag", true), Cnd.where("id","=",oneId));
+            	/*groupPricesettingService.delete(oneId);*/
+            	groupPricesettingService.dao().clear("lo_group_pricesetting", Cnd.where("id", "=", oneId));
                 req.setAttribute("id", oneId);
             }
             return Result.success("system.success");
@@ -131,21 +146,22 @@ public class LosysLogisticsController {
     }
     
 	/**
-	 * 修改物流公司
+	 * 修改价格分组
 	 */
     @At
     @Ok("json")
-    @RequiresPermissions("logistics.manage.edit")
-    @SLog(tag = "修改物流公司", msg = "商品名称:${args[0].name}")
-	@AdaptBy(type = WhaleAdaptor.class)
-    public Object editDo(@Param("..") Lo_logistics logistics, HttpServletRequest req) {	
+    @RequiresPermissions("groupPricesetting.manage.edit")
+    @SLog(tag = "修改价格分组", msg = "价格分组:${args[0].id}")
+    public Object editDo(@Param("..") Lo_group_pricesetting group_pricesetting, HttpServletRequest req) {	
         try {
-        	logisticsService.update(logistics);
+        	groupPricesettingService.update(group_pricesetting);
             return Result.success("system.success");
         } catch (Exception e) {
             return Result.error("system.error");
         }
     }
+    
+
     
     /**
      * 查询数据
@@ -156,26 +172,7 @@ public class LosysLogisticsController {
     @RequiresAuthentication
     public Object data(@Param("length") int length, @Param("start") int start, @Param("draw") int draw, @Param("::order") List<DataTableOrder> order, @Param("::columns") List<DataTableColumn> columns) {
         Cnd cnd = Cnd.NEW();
-        cnd.and("delFlag", "=", '0');
-        
-        return logisticsService.data(length, start, draw, order, columns, cnd, null);
+        return groupPricesettingService.data(length, start, draw, order, columns, cnd, null);
         
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
