8a65339a8de285d36655470ebb7e48a6874f7736
DESKTOP-4C84N1T\华登文 <1762752916@qq.com>
2018-04-04 10:26:28 +0800

    #4423 - bug - 保价管理

--- src/main/java/cn/wizzer/modules/controllers/platform/losys/LosysInsurancePricesettingController.java
+++ src/main/java/cn/wizzer/modules/controllers/platform/losys/LosysInsurancePricesettingController.java
@@ -54,162 +54,158 @@ import org.nutz.mvc.adaptor.WhaleAdaptor;
 import org.nutz.mvc.annotation.*;
 import com.mysql.fabric.xmlrpc.base.Data;
 
-
 /**
  * 保价管理
- * @author 
+ * 
+ * @author
  *
  */
 
 @IocBean
 @At("/platform/losys/insurance")
-@Filters({@By(type = PrivateFilter.class)})
+@Filters({ @By(type = PrivateFilter.class) })
 public class LosysInsurancePricesettingController {
-    private static final Log log = Logs.get();
-    @Inject
-    LosysLogisticsService logisticsService;
-    @Inject
-    LosysInsuranceService insuranceService;
-    @Inject
-    private LosysInsurancePricesettingService insurancePricesettingService;
+	private static final Log log = Logs.get();
+	@Inject
+	LosysLogisticsService logisticsService;
+	@Inject
+	LosysInsuranceService insuranceService;
+	@Inject
+	private LosysInsurancePricesettingService insurancePricesettingService;
 
-    /**
-     * 访问保价管理模块首页
-     */
-    @At("")
-    @Ok("beetl:/platform/losys/insurance/index.html")
-    @RequiresAuthentication
-    public Object index(HttpServletRequest req) {
-    	return logisticsService.query();
-    }
-    
-    /**
-     * 访问保价管理模块添加保价页面
-     */
-    @At
-    @Ok("beetl:/platform/losys/insurance/add.html")
-    @RequiresAuthentication
-    public Object add() {
-    	return logisticsService.query();
-    }
+	/**
+	 * 访问保价管理模块首页
+	 */
+	@At("")
+	@Ok("beetl:/platform/losys/insurance/index.html")
+	@RequiresAuthentication
+	public Object index(HttpServletRequest req) {
+		return logisticsService.query();
+	}
 
-    /**
-     * 访问保价管理模块修改保价页面
-     */
-    @At("/edit/?/?")
-    @Ok("beetl:/platform/losys/insurance/edit.html")
-    @RequiresPermissions("support.price.edit")
-    public Object edit(String insuranceid,String id,HttpServletRequest req) {
-    	List<Lo_insurance> list=insuranceService.query(Cnd.where("id", "=", insuranceid));
-    	for(Lo_insurance data:list){
-    		List<Lo_logistics> logistics=logisticsService.query(Cnd.where("id", "=", data.getLogisticsId()));
-    		for(Lo_logistics lo:logistics){
-    			req.setAttribute("logistics", lo);
-    		}
-    		req.setAttribute("logisticsList", logisticsService.query());
-    	}
-    	return insurancePricesettingService.fetch(id);
-    }
-    
-    /**
-     * 添加保价
-     */
+	/**
+	 * 访问保价管理模块添加保价页面
+	 */
 	@At
-    @Ok("json")
-    @RequiresPermissions("support.price.add")
-    @SLog(tag = "添加保价", msg = "价格保价id:${args[0].id}")
+	@Ok("beetl:/platform/losys/insurance/add.html")
+	@RequiresAuthentication
+	public Object add() {
+		return logisticsService.query();
+	}
+
+	/**
+	 * 访问保价管理模块修改保价页面
+	 */
+	@At("/edit/?/?")
+	@Ok("beetl:/platform/losys/insurance/edit.html")
+	@RequiresPermissions("support.price.edit")
+	public Object edit(String insuranceid, String id, HttpServletRequest req) {
+		List<Lo_insurance> list = insuranceService.query(Cnd.where("id", "=", insuranceid));
+		for (Lo_insurance data : list) {
+			List<Lo_logistics> logistics = logisticsService.query(Cnd.where("id", "=", data.getLogisticsId()));
+			for (Lo_logistics lo : logistics) {
+				req.setAttribute("logistics", lo);
+			}
+			req.setAttribute("logisticsList", logisticsService.query());
+		}
+		return insurancePricesettingService.fetch(id);
+	}
+
+	/**
+	 * 添加保价
+	 */
+	@At
+	@Ok("json")
+	@RequiresPermissions("support.price.add")
+	@SLog(tag = "添加保价", msg = "价格保价id:${args[0].id}")
 	@AdaptBy(type = WhaleAdaptor.class)
-    public Object addDo(@Param("..") Lo_insurance_pricesetting insurance_pricesetting, HttpServletRequest req) {
-        try {
-        	Lo_insurance insurance =new Lo_insurance();
-        	insurance.setLogisticsId(insurance_pricesetting.getInsuranceId());
-        	insuranceService.insert(insurance);
-        	if(insurance_pricesetting.getInsuranceId()==null){
-        		insurance_pricesetting.setOpAt((int)System.currentTimeMillis());
-        		insurance_pricesetting.setInsuranceId(insurance.getId());
-        		insurancePricesettingService.insert(insurance_pricesetting);
-        	}else{
-        		List<Lo_insurance> list=insuranceService.query(Cnd.where("logisticsId", "=", insurance_pricesetting.getInsuranceId()));
-        		for(Lo_insurance data:list){
-        			insurance_pricesetting.setOpAt((int)System.currentTimeMillis());
-        			insurance_pricesetting.setInsuranceId(data.getId());
-        			insurancePricesettingService.insert(insurance_pricesetting);
-        		}
-        	}
-            return Result.success("system.success");
-        } catch (Exception e) {
-            return Result.error("system.error");
-        }
-    }
-    
-    
-    
-    /**
-     * 删除  价格分组
-     */
-    @At({"/delete/?", "/delete"})
-    @Ok("json")
-    @RequiresPermissions("support.price.delete")
-    public Object delete(String oneId, @Param("ids") String[] ids, HttpServletRequest req) {
-    	try {
-        	if (ids != null && ids.length > 0) {
-            	for (String string : ids) {
-            		/*groupPricesettingService.delete(string);*/
-            		insurancePricesettingService.dao().clear("lo_insurance_pricesetting", Cnd.where("id", "=", string));
-            	}
-                req.setAttribute("id", org.apache.shiro.util.StringUtils.toString(ids));
-            } else {
-            	/*groupPricesettingService.delete(oneId);*/
-            	insurancePricesettingService.dao().clear("lo_insurance_pricesetting", Cnd.where("id", "=", oneId));
-                req.setAttribute("id", oneId);
-            }
-            return Result.success("system.success");
-        } catch (Exception e) {
-            return Result.error("system.error");
-        }
-    }
-    
+	public Object addDo(@Param("..") Lo_insurance_pricesetting insurance_pricesetting, HttpServletRequest req) {
+		try {
+			Lo_insurance insurance = new Lo_insurance();
+			insurance.setLogisticsId(insurance_pricesetting.getInsuranceId());
+			insuranceService.insert(insurance);
+			if (insurance_pricesetting.getInsuranceId() == null) {
+				insurance_pricesetting.setOpAt((int) System.currentTimeMillis());
+				insurance_pricesetting.setInsuranceId(insurance.getId());
+				insurancePricesettingService.insert(insurance_pricesetting);
+			} else {
+				Lo_insurance list = insuranceService.fetch(insurance.getId());
+				insurance_pricesetting.setOpAt((int) System.currentTimeMillis());
+				insurance_pricesetting.setInsuranceId(list.getId());
+				insurancePricesettingService.insert(insurance_pricesetting);
+			}
+			return Result.success("system.success");
+		} catch (Exception e) {
+			return Result.error("system.error");
+		}
+	}
+
+	/**
+	 * 删除 价格分组
+	 */
+	@At({ "/delete/?", "/delete" })
+	@Ok("json")
+	@RequiresPermissions("support.price.delete")
+	public Object delete(String oneId, @Param("ids") String[] ids, HttpServletRequest req) {
+		try {
+			if (ids != null && ids.length > 0) {
+				for (String string : ids) {
+					/* groupPricesettingService.delete(string); */
+					insurancePricesettingService.dao().clear("lo_insurance_pricesetting", Cnd.where("id", "=", string));
+				}
+				req.setAttribute("id", org.apache.shiro.util.StringUtils.toString(ids));
+			} else {
+				/* groupPricesettingService.delete(oneId); */
+				insurancePricesettingService.dao().clear("lo_insurance_pricesetting", Cnd.where("id", "=", oneId));
+				req.setAttribute("id", oneId);
+			}
+			return Result.success("system.success");
+		} catch (Exception e) {
+			return Result.error("system.error");
+		}
+	}
+
 	/**
 	 * 修改价格分组
 	 */
-    @At
-    @Ok("json")
-    @RequiresPermissions("support.price.edit")
-    @SLog(tag = "修改保价", msg = "保价:${args[0].id}")
-    public Object editDo(@Param("..") Lo_insurance_pricesetting insurance_pricesetting, HttpServletRequest req) {	
-        try {
-        	List<Lo_insurance> list=insuranceService.query(Cnd.where("logisticsId", "=", insurance_pricesetting.getInsuranceId()));
-        	for(Lo_insurance data:list){
-        		insurance_pricesetting.setOpAt((int)System.currentTimeMillis());
-        		insurance_pricesetting.setInsuranceId(data.getId());
-        		insurancePricesettingService.update(insurance_pricesetting);
-        	}
-            return Result.success("system.success");
-        } catch (Exception e) {
-            return Result.error("system.error");
-        }
-    }
-    
+	@At
+	@Ok("json")
+	@RequiresPermissions("support.price.edit")
+	@SLog(tag = "修改保价", msg = "保价:${args[0].id}")
+	public Object editDo(@Param("..") Lo_insurance_pricesetting insurance_pricesetting, HttpServletRequest req) {
+		try {
+			List<Lo_insurance> list = insuranceService
+					.query(Cnd.where("logisticsId", "=", insurance_pricesetting.getInsuranceId()));
+			for (Lo_insurance data : list) {
+				insurance_pricesetting.setOpAt((int) System.currentTimeMillis());
+				insurance_pricesetting.setInsuranceId(data.getId());
+				insurancePricesettingService.update(insurance_pricesetting);
+			}
+			return Result.success("system.success");
+		} catch (Exception e) {
+			return Result.error("system.error");
+		}
+	}
 
-    
-    /**
-     * 查询数据
-     */
-    
-    @At
-    @Ok("json:full")
-    @RequiresAuthentication
-    public Object data(@Param("logisticsid") String logisticsid,@Param("length") int length, @Param("start") int start, @Param("draw") int draw, @Param("::order") List<DataTableOrder> order, @Param("::columns") List<DataTableColumn> columns) {
-        List<Lo_insurance> list=insuranceService.query();
-        if(list.isEmpty()){
-        	Lo_insurance insurance =new Lo_insurance();
-        	insurance.setLogisticsId(logisticsid);
-        	insuranceService.insert(insurance);
-        }
-        Sql sql = insurancePricesettingService.getMessageList(logisticsid);
+	/**
+	 * 查询数据
+	 */
+
+	@At
+	@Ok("json:full")
+	@RequiresAuthentication
+	public Object data(@Param("logisticsid") String logisticsid, @Param("length") int length, @Param("start") int start,
+			@Param("draw") int draw, @Param("::order") List<DataTableOrder> order,
+			@Param("::columns") List<DataTableColumn> columns) {
+		List<Lo_insurance> list = insuranceService.query();
+		if (list.isEmpty()) {
+			Lo_insurance insurance = new Lo_insurance();
+			insurance.setLogisticsId(logisticsid);
+			insuranceService.insert(insurance);
+		}
+		Sql sql = insurancePricesettingService.getMessageList(logisticsid);
 		return insurancePricesettingService.data(length, start, draw, sql, sql);
-        
-    }
-    
-    
+
+	}
+
 }
